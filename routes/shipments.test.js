0c9a520a6183bc72f233e9927e3f070c77ff743a
MKim <mrn.kim07@gmail.com>
2023-05-16 16:37:40 -0700

    finish part 1 and get code review

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -3,7 +3,6 @@
 const request = require("supertest");
 const app = require("../app");
 
-
 describe("POST /", function () {
   test("valid", async function () {
     const resp = await request(app).post("/shipments").send({
@@ -17,9 +16,23 @@ describe("POST /", function () {
   });
 
   test("throws error if empty request body", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send();
+    const resp = await request(app).post("/shipments").send();
     expect(resp.statusCode).toEqual(400);
+  });
+
+  test("invalid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 800,
+      name: 12345,
+      addr: 12345,
+      zip: 12345,
+    });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message).toEqual([
+      "instance.productId must be greater than or equal to 1000",
+      "instance.name is not of a type(s) string",
+      "instance.addr is not of a type(s) string",
+      "instance.zip is not of a type(s) string",
+    ]);
   });
 });
