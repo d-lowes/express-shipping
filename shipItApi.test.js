2f850793d74933b0c56267bb08583c57accd1575
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-05-16 17:50:45 -0700

    write api tests and mocks

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,32 @@
 "use strict";
 
+const AxiosMockAdapter = require(
+  "axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
 const {
   shipProduct,
+  SHIPIT_SHIP_URL
 } = require("./shipItApi");
 
-
 test("shipProduct", async function () {
-  const shipId = await shipProduct({
-    productId: 1000,
-    name: "Test Tester",
-    addr: "100 Test St",
-    zip: "12345-6789",
+  axiosMock.onPost(SHIPIT_SHIP_URL).reply(200, {
+    "receipt": {
+      "itemId": 1001,
+      "name": "dylan",
+      "addr": "somewhere",
+      "zip": "99999",
+      "shipId": 1100
+    }
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  const shipId = await shipProduct({
+    productId: 1000,
+    name: "dylan",
+    addr: "somewhere",
+    zip: "99999",
+  });;
+
+  expect(shipId).toEqual(1100);
 });
